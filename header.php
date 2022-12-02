5356d8718935c5047331caaf9e43ae1e5da5dff7
atiq-rahman <atiquer@openrefactory.com>
2022-12-02 10:57:49 +0600

    Code Clean

---
+++ header.php
@@ -0,0 +1,202 @@
+<header class="site-header">
+            <div class="container">
+                <div class="site-header__flex">
+                    <div class="site-header__logo">
+                        <a href="index.html">
+                            <img src="wp-content/themes/devsavvy/images/logo.png" alt="Hydeman Development Logo"
+                                width="380" height="80" />
+                        </a>
+                    </div>
+                    <div class="site-header__header-right">
+                        <div class="site-header__phone">
+                            <a href="tel:+1 (306) 537-5656">+1 (306) 537-5656</a>
+                        </div>
+                        <nav class="site-header__nav">
+                            <div class="menu-top-nav-container">
+                                <ul id="menu-top-nav" class="menu">
+                                    <li id="menu-item-395"
+                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-395">
+                                        <a href="our-story/index.html">About Us</a>
+                                        <ul class="sub-menu">
+                                            <li id="menu-item-399"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-399">
+                                                <a href="our-story/index.html">Our Story</a></li>
+                                            <li id="menu-item-396"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-396">
+                                                <a href="our-team/index.html">Team</a></li>
+                                            <!-- <li id="menu-item-396"
+                                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-396">
+                                            <a href="our-team/index.html">News</a></li> -->
+                                        </ul>
+                                    </li>
+                                    <li id="menu-item-388"
+                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-388">
+                                        <a href="commercial-brokerage/index.html">Services</a>
+                                        <ul class="sub-menu">
+                                            <li id="menu-item-389"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-389">
+                                                <a href="commercial-brokerage/index.html">Commercial Brokerage</a>
+                                                <div>
+                                                    <ul class="sub-sub-menu">
+                                                        <li
+                                                          id="menu-item-389"
+                                                          class="menu-item menu-item-type-post_type menu-item-object-page menu-item-389"
+                                                        >
+                                                          <a href="commercial-brokerage/index.html"
+                                                            >Commercial Brokerage</a
+                                                          >
+                                                        </li>
+                                                        <li
+                                                          id="menu-item-390"
+                                                          class="menu-item menu-item-type-post_type menu-item-object-page menu-item-390"
+                                                        >
+                                                          <a href="property-management/index.html"
+                                                            >Property Management</a
+                                                          >
+                                                        </li>
+                                                      </ul>
+                                                  </div>
+                                            </li>
+                                            <li id="menu-item-390"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-390">
+                                                <a href="property-management/index.html">Property Management</a></li>
+                                            <li id="menu-item-391"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-391">
+                                                <a href="asset-management-financial-services/index.html">Asset
+                                                    Management &#038; Financial Services</a>
+                                                
+                                            </li>
+                                            <li id="menu-item-392"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-392">
+                                                <a href="construction-project-management/index.html">Construction
+                                                    Project Management</a></li>
+                                            <li id="menu-item-393"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-393">
+                                                <a href="reo-services/index.html">Software Development</a></li>
+                                            <li id="menu-item-394"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-394">
+                                                <a href="development-acquisitions/index.html">Readymade Garments</a></li>
+                                        </ul>
+                                    </li>
+                                    <li id="menu-item-397"
+                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-397">
+                                        <a href="listings/index.html">Investment</a>
+                                        <ul class="sub-menu">
+                                            <li id="menu-item-399"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-399">
+                                                <a href="our-story/index.html">Opportunities</a></li>
+                                            <li id="menu-item-396"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-396">
+                                                <a href="our-team/index.html">Immigration</a></li>
+                                        </ul>
+                                    </li>
+                                    <!-- <li id="menu-item-398"
+                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-398">
+                                        <a href="blog/index.html">News</a>
+                                        <ul class="sub-menu">
+                                            <li id="menu-item-399"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-399">
+                                                <a href="our-story/index.html">Opportunities</a></li>
+                                            <li id="menu-item-396"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-396">
+                                                <a href="our-team/index.html">Immigration</a></li>
+                                        </ul>
+                                    </li> -->
+                                    <li id="menu-item-398"
+                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-398">
+                                        <a href="blog/index.html">Farmland</a>
+                                        <ul class="sub-menu">
+                                            <li id="menu-item-399"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-399">
+                                                <a href="our-story/index.html">Classifications</a>
+                                                <div>
+                                                    <ul class="sub-sub-menu">
+                                                        <li
+                                                            id="menu-item-389"
+                                                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-389"
+                                                        >
+                                                            <a href="commercial-brokerage/index.html"
+                                                            >Wheat</a
+                                                            >
+                                                            <div>
+                                                                <ul class="sub-sub-sub-menu">
+                                                                    <li
+                                                                        id="menu-item-389"
+                                                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-389"
+                                                                    >
+                                                                        <a href="commercial-brokerage/index.html"
+                                                                        >Grade-1</a
+                                                                        >
+                                                                        
+                                                                    </li>
+                                                                    <li
+                                                                        id="menu-item-390"
+                                                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-390"
+                                                                    >
+                                                                        <a href="property-management/index.html"
+                                                                        >Grade-2</a
+                                                                        >
+                                                                    </li>
+                                                                    <li
+                                                                        id="menu-item-390"
+                                                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-390"
+                                                                    >
+                                                                        <a href="property-management/index.html"
+                                                                        >Grade-3</a
+                                                                        >
+                                                                    </li>
+                                                                    <li
+                                                                        id="menu-item-390"
+                                                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-390"
+                                                                    >
+                                                                        <a href="property-management/index.html"
+                                                                        >Grade-4</a
+                                                                        >
+                                                                    </li>
+                                                                    <li
+                                                                        id="menu-item-390"
+                                                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-390"
+                                                                    >
+                                                                        <a href="property-management/index.html"
+                                                                        >Grade-5(Animal Food)</a
+                                                                        >
+                                                                    </li>
+                                                                    </ul>
+                                                                </div>
+                                                        </li>
+                                                        <li
+                                                            id="menu-item-390"
+                                                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-390"
+                                                        >
+                                                            <a href="property-management/index.html"
+                                                            >Bean</a
+                                                            >
+                                                        </li>
+                                                        <li
+                                                            id="menu-item-390"
+                                                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-390"
+                                                        >
+                                                            <a href="property-management/index.html"
+                                                            >Lentil</a
+                                                            >
+                                                        </li>
+                                                        </ul>
+                                                    </div>
+                                            </li>
+                                            <li id="menu-item-396"
+                                                class="menu-item menu-item-type-post_type menu-item-object-page menu-item-396">
+                                                <a href="our-team/index.html">Export</a></li>
+                                        </ul>
+                                    </li>
+                                    <li id="menu-item-537"
+                                        class="menu-item menu-item-type-post_type menu-item-object-page menu-item-537">
+                                        <a href="contact/index.html">Contact</a>
+                                    </li>
+                                    <!--<li id="menu-item-402" class="tenant-popup menu-item menu-item-type-custom menu-item-object-custom menu-item-402"><a href="#">Tenant Login</a></li> -->
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
